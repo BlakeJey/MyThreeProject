50c13563fdddfe2b357dc7ff101f6d5878e8e547
BlakeJey <123645245+BlakeJey@users.noreply.github.com>
2023-02-02 17:40:06 +0300

    Add files via upload

--- Aboba228.py
+++ Aboba228.py
@@ -5,6 +5,31 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ball = GameSprite('balls.png', 200, 200, 4, 50, 50)
+
+ball.reset()
+
+
+
+
+
+
+
+
+
+
+
+
 
 clock = time.Clock()
 FPS = 60
