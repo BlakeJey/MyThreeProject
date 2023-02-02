267917d79b5a110eb52ff00571e0ec51d72da8f9
BlakeJey <123645245+BlakeJey@users.noreply.github.com>
2023-02-02 17:57:00 +0300

    Add files via upload

--- Aboba228.py
+++ Aboba228.py
@@ -15,8 +15,25 @@ class GameSprite(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+player = Player('rak.png', 5, win_height - 300, 80, 40, 120)
 ball = GameSprite('balls.png', 200, 200, 4, 50, 50)
 
+player.update()
+player.reset()
 ball.reset()
 
 
