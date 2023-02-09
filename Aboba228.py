8c56f80438c35c85aa567eb1441d0c18be97c432
BlakeJey <123645245+BlakeJey@users.noreply.github.com>
2023-02-09 18:00:20 +0300

    Projecttwo

--- Aboba228.py
+++ Aboba228.py
@@ -3,8 +3,11 @@ from pygame import *
 back = (153, 255, 153)
 win_width = 600
 win_height = 500
+speed_x = 3
+speed_y = 3
+speed = 5
 window = display.set_mode((win_width, win_height))
-window.fill(back)
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
@@ -29,12 +32,15 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-player = Player('rak.png', 5, win_height - 300, 80, 40, 120)
-ball = GameSprite('balls.png', 200, 200, 4, 50, 50)
+player1 = Player('rak.png', 5, win_height - 300, 10, 40, 120)
+player2 = Player('rak.png', 550, win_height- 300, 10, 40, 120)
+ball = GameSprite('balls.png', 200, 200, speed, 50, 50)
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
-player.update()
-player.reset()
-ball.reset()
+
 
 
 
@@ -51,11 +57,33 @@ ball.reset()
 clock = time.Clock()
 FPS = 60
 game = True
-
+finish = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    
+  
+    if finish != True:
+
+        window.fill(back)
+
+        player1.update_l()
+        player2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x *= -1/0.85
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+        ball.reset()
+        player1.reset()
+        player2.reset()
     display.update()
     clock.tick(FPS)
